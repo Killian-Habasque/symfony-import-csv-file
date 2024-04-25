14c40ef20e63299a4f7504277711a402f2ea5932
Killian Habasque <killian.habasque@gmail.com>
2024-04-25 18:00:09 +0200

    test migrations

---
+++ src/Entity/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getName(): ?int
+    {
+        return $this->name;
+    }
+}
